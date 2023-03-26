dc4ce434ea5b788a1b74e205206daa0a3eef8c0c
Akash Malik <akashmalik463@gmail.com>
2023-03-26 19:16:31 +0530

    Adding my project

---
+++ src/test/java/utils/TestBase.java
@@ -0,0 +1,63 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import pageObjects.LandingPage;
+
+public class TestBase {
+
+	public WebDriver driver;
+	
+	public WebDriver WebDriverManager() throws IOException
+	{
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//test//resources//global.properties");
+		Properties prop = new Properties();
+		prop.load(fis);
+		String url = prop.getProperty("QAUrl");
+		String browser_properties = prop.getProperty("browser");
+		String browser_maven=System.getProperty("browser");
+		// result = testCondition ? value1 : value2
+		
+		String browser = browser_maven!=null ? browser_maven : browser_properties;
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		//WebDriver driver = new ChromeDriver(options); 
+		
+		
+		
+		if(driver == null)
+		{
+			if(browser.equalsIgnoreCase("chrome"))
+			{
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//src//test//resources//chromedriver.exe");
+		driver = new ChromeDriver(options); // driver gets the life
+		
+			}
+			if(browser.equalsIgnoreCase("firefox"))
+			{
+				System.setProperty("webdriver.gecko.driver","//Users//rahulshetty//Downloads//geckodriver 5");
+				driver = new FirefoxDriver();
+			}
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get(url);
+		}
+		
+		return driver;
+		
+	}
+	
+	
+	
+}
+
